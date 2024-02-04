38e5669d7c1d78404c5ac03507d83f15694b01a8
eerieA <ninja.duan@gmail.com>
2024-02-04 01:51:19 -0800

    Added custom widget base class starter

---
+++ Source/UE_CppTest/Private/WidgetBBBase.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE_CppTest/Public/WidgetBBBase.h"
+
+#if WITH_EDITOR
+const FText UWidgetBBBase::GetPaletteCategory() {
+  return NSLOCTEXT("UMG", "CustomPaletteCategory", "CppTestDev!");
+}
+#endif
---
+++ Source/UE_CppTest/Public/WidgetBBBase.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "WidgetBBBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_CPPTEST_API UWidgetBBBase : public UUserWidget {
+public:
+#if WITH_EDITOR
+  virtual const FText GetPaletteCategory() override;
+#endif
+  
+protected:
+
+private:
+  GENERATED_BODY()
+};
